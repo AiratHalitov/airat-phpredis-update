f088253e426e26a4f1ce186aeecbfe078364dc71
Airat Halitov <airat.halitov@gmail.com>
2020-12-16 00:17:15 +0500

    make -j $NP

--- airat-phpredis-update.sh
+++ airat-phpredis-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # don't forget update version!
+# check version from https://github.com/phpredis/phpredis/blob/develop/Changelog.md
 VER=5.3.2
-# download version from https://github.com/phpredis/phpredis/blob/develop/Changelog.md
+NP=$(nproc)
 
 rm -rf phpredis-$VER
 rm -rf $VER.zip
@@ -13,7 +14,7 @@ if [ -f $VER.zip ]; then
     unzip $VER.zip && rm -rf $VER.zip
 
     cd phpredis-$VER
-    phpize && ./configure && make -j2 && sudo make install && echo "Done!"
+    phpize && ./configure && make -j $NP && sudo make install && echo "Done!"
 fi
 
 # copy redis.ini to /etc/php/7.4/apache2/conf.d and update path
