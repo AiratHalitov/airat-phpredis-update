c85de831fc3138f03f25bcc662fea54b11b01dbf
Airat Halitov <airat.halitov@gmail.com>
2020-12-18 10:52:46 +0500

    && echo "Done!"

--- airat-phpredis-update.sh
+++ airat-phpredis-update.sh
@@ -19,10 +19,8 @@ if [ -f $VER.zip ]; then
     unzip $VER.zip > /dev/null 2>&1 && rm -rf $VER.zip
 
     cd phpredis-$VER
-    phpize > /dev/null 2>&1 && ./configure > /dev/null 2>&1 && make -j $NP > /dev/null 2>&1 && sudo make install
+    phpize > /dev/null 2>&1 && ./configure > /dev/null 2>&1 && make -j $NP > /dev/null 2>&1 && sudo make install && echo "Done!"
 
     # check the right path to redis.so in redis.ini
     #cp redis.ini /etc/php/$PHPVER/apache2/conf.d/redis.ini
-
-    echo "Done!"
 fi
